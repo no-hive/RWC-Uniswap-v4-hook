be20c8bf741ecc1d966ba344d2388a89e454ee1b
no-hive <sprutocean@proton.me>
2025-11-07 23:12:28 +0300

    update_2

--- contracts/GetRealPrices.sol
+++ contracts/GetRealPrices.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.19;
+pragma solidity ^0.8.19;
 
 // this library contains functionality to get Chainlink Currencies Prices Data
 library GetRealPricesOfCurrencies {
